810d57037c0fe68a7f97612b5a18d9f3845b5567
Bertrand THOMAS <bertrand@devpro.fr>
2025-10-22 15:29:28 +0200

    Update projects

--- src/_data/website.js
+++ src/_data/website.js
@@ -2,6 +2,7 @@ export default {
   baseUrl: "https://devpro.fr",
   title: "DevPro blog posts",
   subtitle: "Bits of technology, practices, and everything that can make IT simple and accessible.",
+  siteName: "DevPro",
   author: {
     name: "Bertrand Thomas",
     email: "bertrand@devpro.fr",
