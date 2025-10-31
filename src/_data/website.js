23671dbb1ac5653863bc2d81070129a46d6544a8
Bertrand THOMAS <bertrand@devpro.fr>
2025-10-31 16:51:58 +0100

    Add IndexNow

--- src/_data/website.js
+++ src/_data/website.js
@@ -8,5 +8,6 @@ export default {
     email: "bertrand@devpro.fr",
     profile: "/images/bertrand-lecres.jpg",
     preview: "/images/devpro-header.jpg"
-  }
+  },
+  indexNowKey: "527dc10718fd44b580bec46c39d81015"
 };
